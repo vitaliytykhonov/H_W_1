baf72fa04bdd0bf1eea2ba9cfefe31c51924c0e2
Vitaliy Tykhonov <sonechkovitalka@gmail.com>
2021-10-28 15:14:13 +0300

    H_W_1

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     /* 1
+        var a = 10;
+        var b = 0.2D;
+        var w = 1500L;
+        var q = 1.5D;
+        var r  =5;
+        var i = 1.1D;
+        var v = 222.2D;
+        var z = 111;
+        var x = 23;
+        var y = 13;
+        var c = 0.001D;
+
+        System.out.println(a+b*(w/q)*r+i);
+        System.out.println(b+v-i*z);
+        System.out.println(x+y+z*c);
+    */
+
+   /* 2
+    char h = 104;
+    char e = 101;
+    char l = 108;
+    char o = 111;
+    char w = 119;
+    char r = 114;
+    char d = 100;
+    char p = 32;
+    int i=104;
+
+
+        System.out.println((char)i +i);
+        System.out.println(e);
+    */
+
+        /* 3
+        boolean a = 10<9;
+        boolean b = 3==4;
+        boolean c = 2!=2;
+        boolean d = 9>=8;
+        System.out.println("10 < 9 - " + a);
+        System.out.println("3 == 4 - " + b);
+        System.out.println("2 != 2 - " + c);
+        System.out.println("9 => 8 - " + d);
+     */
+
+    }
+}
